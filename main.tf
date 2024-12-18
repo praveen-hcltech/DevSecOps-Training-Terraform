024a5aa30c45b7640065cd7e3c654042af2f5279
praveen-hcltech <praveensriram.s@hcltech.com>
2024-12-18 08:08:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,17 +40,17 @@ resource "aws_security_group" "DevSecOps_sg" {
   vpc_id = aws_vpc.DevSecOps_vpc.id
 
   egress {
-    cidr_blocks = {"0.0.0.0/0"}
+    cidr_blocks = ["0.0.0.0/0"]
     from_port = 0
     to_port = 0
     protocol = "all"
   }
 
   ingress {
-    cidr_blocks = {"0.0.0.0/0"}
-    from_port = 80
-    to_port = 80
-    protocol = "ttp"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
   }
 }
 
