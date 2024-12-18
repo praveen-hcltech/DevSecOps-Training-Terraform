bdf28d9066b1a5026b5847169dbd573cd6b1767c
praveen-hcltech <praveensriram.s@hcltech.com>
2024-12-18 08:20:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,14 @@ resource "aws_vpc" "devsecops_vpc" {
 resource "aws_subnet" "devsecops_subnet_pub" {
   vpc_id = aws_vpc.devsecops_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "devsecops_subnet_pri" {
   vpc_id = aws_vpc.devsecops_vpc.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
