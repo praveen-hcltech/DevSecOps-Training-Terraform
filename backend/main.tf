bfb7cf08ea1f39db4832bee1df93a20c0bf1ba56
praveen-hcltech <praveensriram.s@hcltech.com>
2024-12-25 16:41:34 +0530

    Update main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -1,1 +1,24 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "devsecops-tf-s3"
+  acl = "private"
 
+  versioning {
+    enable = true
+  }
+}
+
+# DynamoDB Table for state locking
+
+resource "aws_dynamo_table" "tf_lock" {
+  name = "tf-table"
+  billing_mode = "PROVISIONED"
+  read_capacity = 20
+  write_capacity = 20
+  hash_key = "UserId"
+  range_key = "Training-TF"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+}
