5e03248c90e352a2d58213f8ecd9c4b1e0f483de
praveen-hcltech <praveensriram.s@hcltech.com>
2024-12-26 14:04:38 +0530

    Update main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -13,7 +13,6 @@ resource "aws_dynamo_table" "tf_lock" {
   name = "tf-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
-  range_key = "Training-TF"
 
   attribute {
     name = "LockID"
